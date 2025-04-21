bffc1b144dec0904f9a1c53a6480f26a48f76835
Henry <hg097870pq@gmail.com>
2025-04-20 21:32:36 -0500

    fixed errors

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,14 @@
-let tiempo = 1200; 
- 
+let tiempo = 600; 
+const btnResult=document.querySelector('.resultadoCuest')
+const modalRes=document.querySelector('.resultadosImpr')
+
+
+const reactiv=document.querySelector('.anhoter')
+
+reactiv.addEventListener('click',()=>{
+     tiempo = 600; 
+
+})
 let display = document.querySelector('.timer');
 setInterval(() => {
     const minutos = Math.floor(tiempo / 60); 
@@ -12,5 +21,8 @@ setInterval(() => {
     if (tiempo < 0) {
         clearInterval();
         display.innerText = "0:00";
+        modalRes.classList.add('activarModal')
+        document.querySelector('.cuestionarioFrm').classList.add('res')
+
     }
 }, 1000);
