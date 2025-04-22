fe6863247e1523cadad763bd6fec975106ce0ae7
Henry <hg097870pq@gmail.com>
2025-04-21 19:47:21 -0500

    aaaa

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ import './src/styleSignIn.js'
 
 import './src/Responsive.js'
 
+let widths=document.querySelector('.widthSpan')
+let heights=document.querySelector('.heigthSpan')
+
+let windowWidth=window.innerWidth
+let windowHeight=window.innerHeight
+  
+widths.innerText=`${windowWidth}`
+heights.innerText=`${windowHeight}`
+
+window.addEventListener('resize',()=>{ 
+    let windowWidth=window.innerWidth
+    let windowHeight=window.innerHeight
+
+
+    widths.innerText=`${windowWidth}`
+    heights.innerText=`${windowHeight}`
+})
